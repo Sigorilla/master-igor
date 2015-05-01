9aeb4c91e149aa7316c1a1713081bae1210a48ad
Igor Stepanov <sigorilla@gmail.com>
2015-05-01 23:15:03 +0300

    change text of main header

--- main/views.py
+++ main/views.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 def home(request):
   obj = {
-    'page_title': 'Master-Igor.com',
+    'page_title': 'Igor Stepanov',
   }
   return render(request, 'index.html', obj)
 
